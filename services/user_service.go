33cd0440bc502614334703ac99f2d1607e6395b6
Osvaldo Abel <osvaldoabel@finchsolucoes.com.br>
2020-10-19 19:45:06 -0300

    Crud in service layer

---
+++ services/user_service.go
@@ -0,0 +1,45 @@
+package services
+
+import (
+	"osvaldoabel/users-api/domain"
+	"osvaldoabel/users-api/repositories"
+)
+
+type UserService struct {
+	User           *domain.User
+	UserRepository repositories.UserRepository
+}
+
+func NewUserService() UserService {
+	return UserService{}
+}
+
+func (u *UserService) Insert() error {
+	_, err := u.UserRepository.Insert(u.User)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (u *UserService) Update(user *domain.User) error {
+	_, err := u.UserRepository.Update(u.User)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (u *UserService) Delete(id string) error {
+	err := u.UserRepository.Delete(id)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
